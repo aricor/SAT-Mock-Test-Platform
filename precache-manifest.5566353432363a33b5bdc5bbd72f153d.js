8bea785e52c1743832cec90c11f22a31a7f5b928
Vi <vivivi0701@gmail.com>
2020-02-13 00:10:11 +0700

    Updates

---
+++ precache-manifest.5566353432363a33b5bdc5bbd72f153d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d301efa34595ae4247621731298677a",
+    "url": "/SAT-Mock-Test-Platform/index.html"
+  },
+  {
+    "revision": "4ee22cc786c43dd7c0d5",
+    "url": "/SAT-Mock-Test-Platform/static/css/main.7089271b.chunk.css"
+  },
+  {
+    "revision": "b7cdcbf8a6bd6786d9e4",
+    "url": "/SAT-Mock-Test-Platform/static/js/2.0f3036b4.chunk.js"
+  },
+  {
+    "revision": "4ee22cc786c43dd7c0d5",
+    "url": "/SAT-Mock-Test-Platform/static/js/main.6331fb32.chunk.js"
+  },
+  {
+    "revision": "9915aa305436b4d4b565",
+    "url": "/SAT-Mock-Test-Platform/static/js/runtime-main.64b00fa8.js"
+  },
+  {
+    "revision": "46919234464f4c971bc9b1759e7869bb",
+    "url": "/SAT-Mock-Test-Platform/static/media/readingSection2a.46919234.png"
+  },
+  {
+    "revision": "5b77f8832c6701b32f077c87c163683f",
+    "url": "/SAT-Mock-Test-Platform/static/media/readingSection2b.5b77f883.png"
+  },
+  {
+    "revision": "6dbc11cbd0c924629b92dde0a7d45048",
+    "url": "/SAT-Mock-Test-Platform/static/media/readingSection5a.6dbc11cb.png"
+  },
+  {
+    "revision": "de0d93c1c7d54c73c06d791896f8fc8f",
+    "url": "/SAT-Mock-Test-Platform/static/media/readingSection5b.de0d93c1.png"
+  }
+]);
